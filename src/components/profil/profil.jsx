e71d8fb7403370ca229d78b14c250a091d54d7e4
diawara24 <mourtallafatydiawara@gmail.com>
2022-05-31 21:29:22 +0200

    profil component

--- src/components/profil/profil.jsx
+++ src/components/profil/profil.jsx
@@ -3,7 +3,29 @@ import React from 'react'
 const Profil = () => {
   return (
     <div className='profile-container'>
-      <img src="https://via.placeholder.com/150" alt="" />
+     <div className='profile-row'>
+        <div>
+          <img src="https://via.placeholder.com/150" alt="" />
+          <h3>Diawara24</h3>
+        </div>
+        <div className='profile-poste'>
+          <div className='poste-title'>
+            <img src="https://via.placeholder.com/50" alt="" />
+            <h4>Lorem ipsum dolor</h4>
+          </div>
+        </div>
+        <div className='profile-poste'>
+          <div className='poste-title'>
+            <img src="https://via.placeholder.com/50" alt="" />
+            <h4>Lorem ipsum dolor</h4>
+          </div>
+        </div>
+     </div>
+     <div className='posteContent'>
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+       Sit doloremque deserunt sed magnam vel voluptate modi labore temporibus odio,
+        commodi et fugit id soluta nostrum maxime provident. Optio, numquam sunt?
+     </div>
     </div>
   )
 }
